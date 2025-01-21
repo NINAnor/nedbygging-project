24a88b666b57b8c790b1b79ce31d0d427802d01c
Taheera Ahmed <taheera.ahmed@nina.no>
2025-01-21 13:15:27 +0100

    Add constants for labels

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+LABELS = {
+    "Uklassifisert": 0,
+    "Bebyggelse/samferdsel": 1,
+    "Dyrket mark": 2,
+    "Grasmark": 3,
+    "Skog": 4,
+    "Hei og åpen vegetasjon": 5,
+    "Lite vegetert mark": 6,
+    "Våtmark": 7,
+    "Vann": 8,
+}
