d09b5f070fac5542067ec9d1a34c8e91d505bd1a
Taheera Ahmed <taheera.ahmed@nina.no>
2025-02-11 13:15:29 +0100

    [REFINE] formatting in monthlysampler.py

--- src/dataset/monthlysampler.py
+++ src/dataset/monthlysampler.py
@@ -1,11 +1,13 @@
+import random
+from pathlib import Path
+
+import numpy as np
 import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader
 from torchgeo.datasets import RasterDataset, stack_samples
 from torchgeo.samplers import RandomGeoSampler, Units
-from torch.utils.data import DataLoader
-from pathlib import Path
-import torch
-import numpy as np
-import random
+
 
 class CustomGeoDataModule(pl.LightningDataModule):
     def __init__(
@@ -23,15 +25,15 @@ class CustomGeoDataModule(pl.LightningDataModule):
         val_transform=None,
     ):
         super().__init__()
-        self.save_hyperparameters(ignore=['train_transform', 'val_transform'])
-        
+        self.save_hyperparameters(ignore=["train_transform", "val_transform"])
+
         # Store parameters
         self.batch_size = batch_size
         self.patch_size = patch_size
-        self.length_train = length_train 
+        self.length_train = length_train
         self.length_validate = length_validate
         self.num_workers = num_workers
-        
+
         self.train_img_path = train_img_path
         self.train_mask_path = train_mask_path
         self.val_img_path = val_img_path
@@ -54,8 +56,12 @@ class CustomGeoDataModule(pl.LightningDataModule):
         """Set up datasets for training and validation."""
         if stage == "fit" or stage is None:
             # Setup training data
-            train_imgs = RasterDataset(paths=str(self.train_img_path), crs="epsg:32633", res=10)
-            train_masks = RasterDataset(paths=str(self.train_mask_path), crs="epsg:32633", res=10)
+            train_imgs = RasterDataset(
+                paths=str(self.train_img_path), crs="epsg:32633", res=10
+            )
+            train_masks = RasterDataset(
+                paths=str(self.train_mask_path), crs="epsg:32633", res=10
+            )
             train_masks.is_image = False
             self.train_dataset = train_imgs & train_masks
 
@@ -63,16 +69,20 @@ class CustomGeoDataModule(pl.LightningDataModule):
                 raise ValueError("Train dataset is empty!")
 
             self.train_sampler = RandomGeoSampler(
-                dataset=self.train_dataset, 
-                size=self.patch_size, 
-                length=self.length_train, 
-                units=Units.PIXELS
+                dataset=self.train_dataset,
+                size=self.patch_size,
+                length=self.length_train,
+                units=Units.PIXELS,
             )
 
         if stage == "validate" or stage is None:
             # Setup validation data
-            val_imgs = RasterDataset(paths=str(self.val_img_path), crs="epsg:32633", res=10)
-            val_masks = RasterDataset(paths=str(self.val_mask_path), crs="epsg:32633", res=10)
+            val_imgs = RasterDataset(
+                paths=str(self.val_img_path), crs="epsg:32633", res=10
+            )
+            val_masks = RasterDataset(
+                paths=str(self.val_mask_path), crs="epsg:32633", res=10
+            )
             val_masks.is_image = False
             self.val_dataset = val_imgs & val_masks
 
@@ -80,17 +90,17 @@ class CustomGeoDataModule(pl.LightningDataModule):
                 raise ValueError("Validation dataset is empty!")
 
             self.val_sampler = RandomGeoSampler(
-                dataset=self.val_dataset, 
-                size=self.patch_size, 
-                length=self.length_validate, 
-                units=Units.PIXELS
+                dataset=self.val_dataset,
+                size=self.patch_size,
+                length=self.length_validate,
+                units=Units.PIXELS,
             )
 
     def train_dataloader(self):
         """Get train dataloader."""
         if self.train_dataset is None:
             raise ValueError("Training dataset is not initialized. Call setup() first")
-            
+
         return DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
@@ -103,8 +113,10 @@ class CustomGeoDataModule(pl.LightningDataModule):
     def val_dataloader(self):
         """Get validation dataloader."""
         if self.val_dataset is None:
-            raise ValueError("Validation dataset is not initialized. Call setup() first")
-            
+            raise ValueError(
+                "Validation dataset is not initialized. Call setup() first"
+            )
+
         return DataLoader(
             self.val_dataset,
             batch_size=self.batch_size,
@@ -130,37 +142,46 @@ class CustomGeoDataModule(pl.LightningDataModule):
         augmented_samples = []
 
         for sample in batch:
-            image = sample['image'].clone().detach()
-            mask = sample['mask'].clone().detach()
+            image = sample["image"].clone().detach()
+            # mask = sample["mask"].clone().detach()
 
             # Select a random month
-            selected_month = random.randint(0, num_months - 1)
+            selected_month = random.randint(0, num_months - 1)  # noqa: S311
             band_start = selected_month * bands_per_month
             band_end = band_start + bands_per_month
 
-            sample['image'] = image[band_start:band_end, :, :]
+            sample["image"] = image[band_start:band_end, :, :]
 
             # Convert tensors to numpy for augmentation
-            image_np = sample['image'].numpy().transpose(1, 2, 0)
-            mask_np = sample['mask'].numpy().astype(np.uint8).transpose(1, 2, 0)
+            image_np = sample["image"].numpy().transpose(1, 2, 0)
+            mask_np = sample["mask"].numpy().astype(np.uint8).transpose(1, 2, 0)
 
             # Apply augmentation if transform is provided
             if transform is not None:
                 augmented = transform(image=image_np, mask=mask_np)
 
                 # Ensure 'augmented' outputs are numpy arrays before conversion
-                if isinstance(augmented['image'], np.ndarray):
-                    sample['image'] = torch.from_numpy(augmented['image']).clone().detach()
+                if isinstance(augmented["image"], np.ndarray):
+                    sample["image"] = (
+                        torch.from_numpy(augmented["image"]).clone().detach()
+                    )
                 else:
-                    sample['image'] = augmented['image'].clone().detach()
+                    sample["image"] = augmented["image"].clone().detach()
 
-                if isinstance(augmented['mask'], np.ndarray):
-                    sample['mask'] = torch.from_numpy(augmented['mask']).clone().detach().permute(2, 0, 1)
+                if isinstance(augmented["mask"], np.ndarray):
+                    sample["mask"] = (
+                        torch.from_numpy(augmented["mask"])
+                        .clone()
+                        .detach()
+                        .permute(2, 0, 1)
+                    )
                 else:
-                    sample['mask'] = augmented['mask'].clone().detach().permute(2, 0, 1)
+                    sample["mask"] = augmented["mask"].clone().detach().permute(2, 0, 1)
             else:
-                sample['image'] = torch.tensor(image_np.transpose(2, 0, 1)).clone().detach()
-                sample['mask'] = torch.tensor(mask_np).clone().detach().permute(2, 0, 1)
+                sample["image"] = (
+                    torch.tensor(image_np.transpose(2, 0, 1)).clone().detach()
+                )
+                sample["mask"] = torch.tensor(mask_np).clone().detach().permute(2, 0, 1)
 
             augmented_samples.append(sample)
 
