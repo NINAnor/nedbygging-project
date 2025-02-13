d99aae00747ccfa37bcdd4053bdc5819920b5dc9
Taheera Ahmed <taheera.ahmed@nina.no>
2025-02-13 15:11:12 +0100

    [ADD] new colors and labels for plotting masks, change default value of bright, change channels

--- src/dataset/visualisation.py
+++ src/dataset/visualisation.py
@@ -2,11 +2,13 @@ from collections.abc import Iterable
 
 import matplotlib.pyplot as plt
 import torch
+from matplotlib.colors import ListedColormap
+from matplotlib.patches import Patch
 from torchgeo.datasets import unbind_samples
 
 
 def plot_imgs(
-    images: Iterable, axs: Iterable, chnls: list[int] | None = None, bright: float = 3.0
+    images: Iterable, axs: Iterable, chnls: list[int] | None = None, bright: float = 0.0
 ):
     # for some reason ruff is complaining about using a "mutable default argument"?
     if chnls is None:  # Initialize the default inside the function
@@ -21,22 +23,34 @@ def plot_imgs(
         img_min, img_max = img.min(), img.max()
         img = (img - img_min) / (img_max - img_min + 1e-8)
 
-        arr = torch.clamp(bright * img, min=0, max=1).numpy()
-        rgb = arr.transpose(0, 1, 2)
+        if bright != 0.0:
+            img_arr = torch.clamp(bright * img, min=0, max=1).numpy()
+        else:
+            img_arr = img.numpy()
+        rgb = img_arr.transpose(0, 1, 2)
 
         ax.imshow(rgb)
         ax.axis("off")
 
 
 def plot_msks(masks: Iterable, axs: Iterable):
+    class_labels = ["urban", "cropland", "grass", "forest", "wetland", "water"]
+    class_colors = ["red", "yellow", "lime", "green", "purple", "blue"]
+
+    legend_elements = [
+        Patch(facecolor=color, edgecolor="black", label=label)
+        for color, label in zip(class_colors, class_labels, strict=False)
+    ]
     for mask, ax in zip(masks, axs, strict=False):
-        ax.imshow(mask.squeeze().numpy(), cmap="Blues")
+        ax.imshow(mask.squeeze().numpy(), cmap=ListedColormap(class_colors))
         ax.axis("off")
+
+    axs[0].legend(handles=legend_elements, loc="upper right", title="Classes")
 
 
 def plot_batch(
     batch: dict,
-    bright: float = 3.0,
+    bright: float = 0.0,
     cols: int = 4,
     width: int = 5,
     chnls: list[int] | None = None,
@@ -82,17 +96,14 @@ def plot_batch(
 
 
 def plotBatch(train_loader, val_loader):
-    print("PLOTTING A BATCH OF IMAGE AND MASKS")
-
     train_batch = next(iter(train_loader))
     val_batch = next(iter(val_loader))
 
-    plot_batch(train_batch, bright=3.0, cols=4, width=5, chnls=[0, 1, 2])
+    plot_batch(train_batch, cols=4, width=5, chnls=[2, 1, 0])
     plt.suptitle("Training Batch")
     plt.savefig("training_batch.png")
 
-    plot_batch(val_batch, bright=3.0, cols=4, width=5, chnls=[0, 1, 2])
+    plot_batch(val_batch, cols=4, width=5, chnls=[2, 1, 0])
     plt.suptitle("Validation Batch")
     plt.savefig("validation_batch.png")
-
     exit()
